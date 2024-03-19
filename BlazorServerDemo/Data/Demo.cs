3735c2fb6faa1a21d13220af46306fe259bebc04
Yassir C <51804657+Klimrod89@users.noreply.github.com>
2024-03-19 18:33:34 -0400

    Learned about Creating a DI registerd to class, Scope

---
+++ BlazorServerDemo/Data/Demo.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorServerDemo;
+
+public class Demo 
+{
+    public DateTime StartupTime { get; init; }   
+    public Demo()
+    {
+        StartupTime = DateTime.Now;
+    }
+}
---
+++ BlazorServerDemo/Data/ProcessDemo.cs
@@ -0,0 +1,31 @@
+﻿namespace BlazorServerDemo;
+
+public class ProcessDemo
+{
+    private readonly Demo demo;
+
+    public ProcessDemo(Demo demo)
+    {
+        this.demo = demo;
+    }
+
+    public int GetDaysInMonth()
+    {
+        return demo.StartupTime.Month switch
+        {
+            1 => 31,
+            2 => 28,
+            3 => 31,
+            4 => 30,
+            5 => 31,
+            6 => 30,
+            7 => 31,
+            8 => 31,
+            9 => 30,
+            10 => 31,
+            11 => 30,
+            12 => 31,
+            _ => throw new IndexOutOfRangeException()
+        };
+    }
+}
