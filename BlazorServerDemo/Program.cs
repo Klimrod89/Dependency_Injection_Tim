204cdc5a1ae39d872fd3518df41754b260c424a4
Yassir C <51804657+Klimrod89@users.noreply.github.com>
2024-03-20 02:25:01 -0400

    Learn about scope in DI

--- BlazorServerDemo/Program.cs
+++ BlazorServerDemo/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
-builder.Services.AddTransient<Demo>();
+builder.Services.AddScoped<Demo>();
 builder.Services.AddTransient<ProcessDemo>();
 
 var app = builder.Build();
